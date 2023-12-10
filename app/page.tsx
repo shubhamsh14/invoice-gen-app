f8dd068c69e14e328ae5404fb5d71750b68620e9
Shubham Shandilya <73842314+shubhamsh14@users.noreply.github.com>
2023-12-11 00:31:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,31 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
-export default function Home() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReportsHome from './components/reportComponent/Reports';
+import Login from "./components/Login";
+import Home from "./components/homeComponent/Home";
+import InvoiceForm from "./components/generateInvoiceComponent/InvoiceForm";
+import InventoryHome from "./components/inventoryComponents/InventoryHome";
+import GST1Report from "./components/reportComponent/GSTReport1"
+import GST2Report from "./components/reportComponent/GSTReport2"
+import GSTPurchase from "./components/reportComponent/GSTPurchase"
+import GSTSales
+ from "./components/reportComponent/GSTSales";
+function App() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <span>+</span>
-        <Image
-          src="/amplify.svg"
-          alt="Amplify Logo"
-          width={45}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://docs.amplify.aws/gen2/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Review documentation for Amplify's code-first DX (Gen 2).</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/start/quickstart/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Quickstart <span>-&gt;</span>
-          </h2>
-          <p>Follow a tutorial to build a fullstack app with Amplify Gen 2.</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/auth/set-up-auth/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Auth <span>-&gt;</span>
-          </h2>
-          <p>Zero-config Auth UI components with social sign-in and MFA.</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/data/set-up-data/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Data <span>-&gt;</span>
-          </h2>
-          <p>
-            Fully-typed real-time API with NoSQL database.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/invoice-gen" element={<InvoiceForm />} />
+        <Route path="/reports" element={<ReportsHome />} />
+        <Route path="/inventory" element={<InventoryHome />} />
+        <Route path="/GST1Report" element={<GST1Report />} />
+        <Route path="/GST2Report" element={<GST2Report />} />
+        <Route path="/GSTPurchase" element={<GSTPurchase />} />
+        <Route path="/GSTSales" element={<GSTSales />} />
+        
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
